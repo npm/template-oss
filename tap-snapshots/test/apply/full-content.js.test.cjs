9a34b0b01e713774037ba80ea2ff971a0569cfbe
Luke Karrys <luke@lukekarrys.com>
2022-08-25 13:55:12 -0700

    fix: default root component to empty string to match previous behavior

--- tap-snapshots/test/apply/full-content.js.test.cjs
+++ tap-snapshots/test/apply/full-content.js.test.cjs
@@ -599,7 +599,9 @@ release-please-config.json
     }
   ],
   "packages": {
-    ".": {}
+    ".": {
+      "package-name": ""
+    }
   }
 }
 `
@@ -1399,7 +1401,9 @@ release-please-config.json
     }
   ],
   "packages": {
-    ".": {},
+    ".": {
+      "package-name": ""
+    },
     "workspaces/a": {},
     "workspaces/b": {}
   }
