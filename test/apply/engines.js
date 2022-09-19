fef07262f84399c3b37824beb1a557cc9e0f144f
Luke Karrys <luke@lukekarrys.com>
2022-09-19 15:31:44 -0700

    fix: workspace can use only latest ci version of root

--- test/apply/engines.js
+++ test/apply/engines.js
@@ -33,3 +33,37 @@ t.test('latest ci versions', async (t) => {
   const pkg = await s.readJson('package.json')
   t.equal(pkg.engines.node, '>=18.0.0')
 })
+
+t.test('latest ci versions in workspace', async (t) => {
+  const s = await setup(t, {
+    package: {
+      templateOSS: {
+        content: 'content',
+        ciVersions: ['12.x', '14.x', '16.x'],
+      },
+    },
+    workspaces: {
+      a: {
+        templateOSS: {
+          ciVersions: 'latest',
+        },
+      },
+    },
+    testdir: {
+      content: {
+        'source.json': '{ "node": {{{ json engines }}} }',
+        'index.js': `module.exports={
+          rootRepo:{add:{'target.json':'source.json'}},
+          workspaceRepo:{add:{'target-{{ pkgNameFs }}.json':'source.json'}}
+        }`,
+      },
+    },
+  })
+  await s.apply()
+
+  const root = await s.readJson('target.json')
+  const workspace = await s.readJson('target-a.json')
+
+  t.equal(root.node, '^12.0.0 || ^14.0.0 || >=16.0.0')
+  t.equal(workspace.node, '>=16.0.0')
+})
