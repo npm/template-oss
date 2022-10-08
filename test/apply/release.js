bc72b05884dc59cd5f45b6f29f3308aad0456e7a
Luke Karrys <luke@lukekarrys.com>
2022-10-07 23:08:08 -0700

    fix: add individual workspace flag to lint step

--- test/apply/release.js
+++ test/apply/release.js
@@ -6,9 +6,9 @@ t.test('no workspace flags in commands', async (t) => {
   const s = await setup(t)
   await s.apply()
 
-  const release = await s.readFile(join('.github', 'workflows', 'release.yml'))
+  const release = await s.readFile(join('.github', 'workflows', 'ci-release.yml'))
 
-  t.match(release, '--ignore-scripts --if-present\n')
+  t.match(release, '--ignore-scripts\n')
   t.notMatch(release, '--ignore-scripts -ws -iwr --if-present\n')
 })
 
@@ -20,8 +20,8 @@ t.test('uses workspace flags in commands', async (t) => {
   })
   await s.apply()
 
-  const release = await s.readFile(join('.github', 'workflows', 'release.yml'))
+  const release = await s.readFile(join('.github', 'workflows', 'ci-release.yml'))
 
-  t.notMatch(release, '--ignore-scripts --if-present\n')
+  t.notMatch(release, '--ignore-scripts\n')
   t.match(release, '--ignore-scripts -ws -iwr --if-present\n')
 })
