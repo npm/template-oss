c955b3eab79a220d455ed96495157ca29f008a6d
Luke Karrys <luke@lukekarrys.com>
2022-10-25 13:00:20 -0700

    chore: add tests for release specifics

--- test/apply/release-config.js
+++ test/apply/release-config.js
@@ -1,36 +1,28 @@
 const t = require('tap')
+const { existsSync } = require('fs')
 const setup = require('../setup.js')
 
-t.test('no plugin for root only', async (t) => {
-  const s = await setup(t)
-  await s.apply()
+t.test('root only', async (t) => {
+  const prvt = { private: true }
+  const cases = [
+    [{}, { plugins: false, pr: true }],
+    [{ workspaces: { a: 'a' } }, { plugins: true, pr: true }],
+    [{ workspaces: { a: prvt } }, { plugins: false, pr: true }],
+    [{ package: prvt }, { plugins: false, pr: false }],
+    [{ package: prvt, workspaces: { a: 'a' } }, { plugins: true, pr: true }],
+    [{ package: prvt, workspaces: { a: prvt } }, { plugins: false, pr: false }],
+  ]
 
-  const releaseConfig = await s.readJson('release-please-config.json')
-  t.equal(releaseConfig.plugins, undefined)
-})
+  for (const [config, expected] of cases) {
+    await t.test(JSON.stringify(config), async t => {
+      const s = await setup(t, config)
+      await s.apply()
 
-t.test('has plugin for workspace', async (t) => {
-  const s = await setup(t, {
-    workspaces: {
-      a: 'a',
-    },
-  })
-  await s.apply()
+      const releaseConfig = await s.readJson('release-please-config.json').catch(() => ({}))
+      const pr = existsSync(s.join('.github', 'workflows', 'pull-request.yml'))
 
-  const releaseConfig = await s.readJson('release-please-config.json')
-  t.strictSame(releaseConfig.plugins, ['node-workspace'])
-})
-
-t.test('no plugin for private workspace', async (t) => {
-  const s = await setup(t, {
-    workspaces: {
-      a: {
-        private: true,
-      },
-    },
-  })
-  await s.apply()
-
-  const releaseConfig = await s.readJson('release-please-config.json')
-  t.equal(releaseConfig.plugins, undefined)
+      t.strictSame(releaseConfig.plugins, expected.plugins ? ['node-workspace'] : undefined)
+      t.equal(pr, expected.pr)
+    })
+  }
 })
