3fcf46c801d0a86e0de90b235e0daa3f028a828e
Luke Karrys <luke@lukekarrys.com>
2022-11-10 10:54:17 -0700

    feat: add npmSpec config item

--- test/check/dogfood.js
+++ test/check/dogfood.js
@@ -6,4 +6,5 @@ t.test('this repo passes all checks', async (t) => {
   const root = resolve(__dirname, '..', '..')
   const res = await check(root)
   t.equal(res.length, 0)
+  t.strictSame(res, [])
 })
