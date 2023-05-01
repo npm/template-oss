0f5932aefe7fa785a57df8eaac8a041035bb289b
Gar <gar+gh@danger.computer>
2023-05-01 07:21:43 -0700

    chore: fix tests for node v20

--- test/bin/apply.js
+++ test/bin/apply.js
@@ -27,21 +27,21 @@ t.afterEach(() => {
 
 t.test('when npm_config_local_prefix is unset, does nothing', async (t) => {
   await templateApply()
-  t.equal(process.exitCode, undefined, 'exitCode is unset')
+  t.notOk(process.exitCode, 'exitCode is unset')
 })
 
 t.test('when npm_config_global is true, does nothing', async (t) => {
   process.env.npm_config_global = 'true'
 
   await templateApply()
-  t.equal(process.exitCode, undefined, 'exitCode is unset')
+  t.notOk(process.exitCode, 'exitCode is unset')
 })
 
 t.test('with mocks', async (t) => {
   process.env.npm_config_local_prefix = 'heynow'
 
   await templateApply(() => {})
-  t.equal(process.exitCode, undefined, 'exitCode is unset')
+  t.notOk(process.exitCode, 'exitCode is unset')
 })
 
 t.test('error', async (t) => {
@@ -50,5 +50,5 @@ t.test('error', async (t) => {
   await templateApply(() => {
     throw new Error('apply')
   })
-  t.equal(process.exitCode, 1, 'exitCode is unset')
+  t.ok(process.exitCode, 'exitCode is unset')
 })
