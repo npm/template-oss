5909303979852539471fef8aef6ef27afd00b386
Luke Karrys <luke@lukekarrys.com>
2024-04-15 12:47:35 -0700

    fix: turn of body-max-line-length commitlint rule

--- tap-snapshots/test/apply/source-snapshots.js.test.cjs
+++ tap-snapshots/test/apply/source-snapshots.js.test.cjs
@@ -15,7 +15,8 @@ module.exports = {
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'deps', 'chore']],
     'header-max-length': [2, 'always', 80],
-    'subject-case': [0, 'always', ['lower-case', 'sentence-case', 'start-case']],
+    'subject-case': [0],
+    'body-max-line-length': [0],
   },
 }
 
@@ -1448,7 +1449,8 @@ module.exports = {
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'deps', 'chore']],
     'header-max-length': [2, 'always', 80],
-    'subject-case': [0, 'always', ['lower-case', 'sentence-case', 'start-case']],
+    'subject-case': [0],
+    'body-max-line-length': [0],
   },
 }
 
