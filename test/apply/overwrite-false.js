f32edd20d8ba7bcf789b4703ce183f58e5e9c06f
Luke Karrys <luke@lukekarrys.com>
2024-05-02 12:27:59 -0700

    fix: properly check for diff with multiple files from overwrite:false
    Fixes #437

--- test/apply/overwrite-false.js
+++ test/apply/overwrite-false.js
@@ -31,10 +31,31 @@ t.test('json merge', async (t) => {
   })
 
   await s.apply()
+  t.strictSame(await s.check(), [])
 
   const pkg = await s.readJson('package.json')
   t.equal(pkg.scripts.test, 'tap test/')
   t.equal(pkg.scripts.snap, 'tap')
 
+  await s.writeJson('package.json', {
+    ...pkg,
+    author: 'What?',
+    scripts: {
+      ...pkg.scripts,
+      test: 'tap',
+    },
+    templateOSS: {
+      ...pkg.templateOSS,
+      version: '1.0.0',
+    },
+  })
+
+  const checks = await s.check()
+  t.equal(checks.length, 1)
+  t.match(checks[0].title, 'package.json needs to be updated')
+  t.match(checks[0].body[0], `"author" is "What?", expected "GitHub Inc."`)
+  t.match(checks[0].body[0], `scripts.test" is "tap", expected "tap test/"`)
+
+  await s.apply()
   t.strictSame(await s.check(), [])
 })
